d606745b3e888235500e7dad8033556c6724aef6
AALEKH <aalekh.nigam@gmail.com>
2019-08-30 00:05:34 +0530

    Implemented N Queens problem using Backtracking

---
+++ n_queens.py
@@ -0,0 +1,25 @@
+# Implementing solution via Backtracking
+
+def can_be_extended_to_solution(perm):
+	i = len(perm)-1
+	for j in range(i):
+		# If two queens happens to intersect
+		if i-j == abs(perm[i]-perm[j]):
+			return False
+		return True
+
+def extend(perm, n):
+	if len(perm) == n:
+		print(perm)
+		exit()
+	
+	for k in range(n):
+		if k not in perm:
+			perm.append(k)
+
+			if can_be_extended_to_solution(perm):
+				extend(perm, n)
+			
+			perm.pop()
+
+extend(perm = [], n = 20)
