042606b61cd8085db00de94185d42777e4f32a3a
AALEKH <aalekh.nigam@gmail.com>
2020-04-01 23:15:45 +0530

    Adding Solution for 1119 [LC]

---
+++ removing_vowels.cpp
@@ -0,0 +1,14 @@
+# Remove Vowels from a String
+
+class Solution {
+public:
+    string removeVowels(string S) {
+        string answer = "";
+        for (char c: S){
+            if(!(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')){
+                answer += c;
+            }
+        }
+        return answer;
+    }
+};
